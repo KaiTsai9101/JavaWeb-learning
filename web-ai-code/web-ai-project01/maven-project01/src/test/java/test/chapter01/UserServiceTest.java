7ff3446ac72554668f7de86e88fdddc7c0816193
Kai Tsai <105984936+KaiTsai9101@users.noreply.github.com>
2026-05-06 00:47:38 +0800

    Add files via upload

--- web-ai-code/web-ai-project01/maven-project01/src/test/java/test/chapter01/UserServiceTest.java
+++ web-ai-code/web-ai-project01/maven-project01/src/test/java/test/chapter01/UserServiceTest.java
@@ -1,9 +1,13 @@
 package test.chapter01;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 // 测试类
+@DisplayName("用户信息的测试类")
 public class UserServiceTest {
 
     @Test
@@ -34,5 +38,15 @@ public class UserServiceTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
            userService.getGender(null);
         });
+    }
+
+    // 参数化测试
+    @DisplayName("测试用户性别")
+    @ParameterizedTest
+    @ValueSource(strings = {"100000200010011011", "100000200010011031", "100000200010011051"})
+    public void testGetGender2(String idCard){
+        UserService userService = new UserService();
+        String gender = userService.getGender(idCard);
+        Assertions.assertEquals("男", gender, "性别获取错误");
     }
 }
